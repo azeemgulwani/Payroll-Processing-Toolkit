9eaa7986e508d4409205bff3468c24f9e3f116da
Azeem Gulwani <azeemgulwani@usf.edu>
2025-09-28 01:01:33 -0400

    created array of employees

--- cc_5.js
+++ cc_5.js
@@ -1,2 +1,10 @@
 // Coding Challenge 5
 
+// Step 2:
+const employees = [
+  { name: "Ava",    hourlyRate: 22.5, hoursWorked: 38 },
+  { name: "Ben",    hourlyRate: 18.0, hoursWorked: 42 },
+  { name: "Chloe",  hourlyRate: 30.0, hoursWorked: 45 },
+  { name: "Diego",  hourlyRate: 16.75, hoursWorked: 40 },
+  { name: "Esha",   hourlyRate: 25.0, hoursWorked: 50 }
+]
