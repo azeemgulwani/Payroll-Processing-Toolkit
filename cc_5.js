884107671c56938e8aaaa20eb8b34f15496e7a9a
Azeem Gulwani <azeemgulwani@usf.edu>
2025-09-28 01:24:42 -0400

    Created function to calculate basepay

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,15 @@ const employees = [
   { name: "Diego",  hourlyRate: 16.75, hoursWorked: 40 },
   { name: "Esha",   hourlyRate: 25.0, hoursWorked: 50 }
 ]
+
+// Step 3:
+
+function calculateBasePay(rate, hours){
+  let baseHours = hours;
+  if (baseHours > 40){
+    baseHours = 40;
+  }
+  let pay = rate * baseHours;
+  return pay.toFixed(2);
+}
+console.log(calculateBasePay(20, 35));
